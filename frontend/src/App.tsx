5b259a619c7aa6274a6c1d059d25564b40def6df
KamijoKeiko <0402576@mchcgr.com>
2023-12-06 18:40:01 +0900

    submit file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
-type ApiResponse = {
-    message: string
+type User = {
+    name: string;
+    department: string;
 };
-export const App:React.FC = () => {
-     const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        axios.get<ApiResponse>('http://localhost:7000/hello')
-            .then(response => {
-                setMessage(response.data.message);
-            })
-            .catch(error => console.error('Error:', error));
-    }, []);
+export const App: React.FC = () => {
+    const [user, setUser] = useState<User>({name: "", department: ""});
+    const [file, setFile] = useState<File | null>(null)
+    const handleClick = async () => {
+        if (!file) return;
+
+        const formData = new FormData();
+        formData.append('name', user.name)
+        formData.append('department', user.department)
+        formData.append('file', file)
+
+        try {
+            const response = await axios.post('http://localhost:7000/submit', formData);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
 
     return (
-        <div>
-            <h1>Response from API</h1>
-            <h2>{message}</h2>
-        </div>
+        <><input
+            type="text"
+            value={user.name}
+            onChange={(e) => setUser({...user, name: e.target.value})}
+            placeholder="Name"
+        />
+            <input
+                type="text"
+                value={user.department}
+                onChange={(e) => setUser({...user, department: e.target.value})}
+                placeholder="Department"
+            />
+            <input
+                type="file"
+                onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
+            />
+
+            <button onClick={handleClick}>Submit</button>
+        </>
     );
-}
+};
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+import base64
+
+from fastapi import FastAPI, UploadFile, Form, File
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -27,3 +30,28 @@ async def say_hello():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+class User(BaseModel):
+    name: str
+    department: str
+
+
+# 仮のデータストア
+data_store = []
+
+
+@app.post("/submit")
+async def submit(name: str = Form(...), department: str = Form(...), file: UploadFile = File(...)):
+    user_data = {"name": name, "department": department}
+    file_content = await file.read()
+    file_content_base64 = base64.b64encode(file_content)
+    data_store.append(user_data)
+    data_store.append(file_content_base64)
+
+    return {"message": "User added successfully"}
+
+
+@app.get("/data")
+async def get_data():
+    return data_store
